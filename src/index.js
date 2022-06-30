0483eb4bfd5984fb1b1e1177dc97f146efc6d8ab
Nick Warren <nickwarren47@gmail.com>
2022-06-29 18:05:20 -0600

    Donw with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createTasks(e.target.newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function createTasks(list){
+  let p = document.createElement('p')
+  let btn = document.createElement("button")
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${list} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
